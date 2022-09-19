0c09fdd01f083812a0c6ba9fb4e579790ca136ad
PC <PC@DESKTOP-6N49J6I>
2022-09-19 02:02:22 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio3/Exercicio3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    class Exercicio3
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Digite seu nome:");
+            string nome = Console.ReadLine();
+            nome = nome.ToUpper();
+            Console.WriteLine($"Seu nome no crachá é: {Abreviatte($"{nome}")}");
+            Console.WriteLine("Pressione qualquer tecla para sair!!!");
+            
+        }
+
+        static string Abreviatte(string nome)
+        {
+            var meio = " ";
+            var nomes = nome.Split(' ');
+            for (var i = 0;  i < nomes.Length - 1; i++)
+            {
+                if (!nomes[i].Equals("de", StringComparison.OrdinalIgnoreCase) &&
+                    !nomes[i].Equals("da", StringComparison.OrdinalIgnoreCase) &&
+                    !nomes[i].Equals("do", StringComparison.OrdinalIgnoreCase) &&
+                    !nomes[i].Equals("das", StringComparison.OrdinalIgnoreCase) &&
+                    !nomes[i].Equals("dos", StringComparison.OrdinalIgnoreCase) &&
+                    !nomes[i].Equals("e", StringComparison.OrdinalIgnoreCase))
+                    meio += nomes[i][0] + ". ";
+                      
+            }
+            return nomes[nomes.Length - 1] + "," + meio;
+
+
+
+
+
+        }
+
+
+    }
+}
